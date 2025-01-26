a9d68553aef778083ffdaa884d62424aa3869cb8
Sid <tosid23@gmail.com>
2025-01-26 12:05:28 +0530

    Add Material Design color constants for red, pink, purple, and deep purple

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -24,3 +24,67 @@ export const Colors = {
     tabIconSelected: tintColorDark,
   },
 };
+
+// Red
+export const md_red_50 = '#FFEBEE';
+export const md_red_100 = '#FFCDD2';
+export const md_red_200 = '#EF9A9A';
+export const md_red_300 = '#E57373';
+export const md_red_400 = '#EF5350';
+export const md_red_500 = '#F44336';
+export const md_red_600 = '#E53935';
+export const md_red_700 = '#D32F2F';
+export const md_red_800 = '#C62828';
+export const md_red_900 = '#B71C1C';
+export const md_red_A100 = '#FF8A80';
+export const md_red_A200 = '#FF5252';
+export const md_red_A400 = '#FF1744';
+export const md_red_A700 = '#D50000';
+
+// Pink
+export const md_pink_50 = '#FCE4EC';
+export const md_pink_100 = '#F8BBD0';
+export const md_pink_200 = '#F48FB1';
+export const md_pink_300 = '#F06292';
+export const md_pink_400 = '#EC407A';
+export const md_pink_500 = '#E91E63';
+export const md_pink_600 = '#D81B60';
+export const md_pink_700 = '#C2185B';
+export const md_pink_800 = '#AD1457';
+export const md_pink_900 = '#880E4F';
+export const md_pink_A100 = '#FF80AB';
+export const md_pink_A200 = '#FF4081';
+export const md_pink_A400 = '#F50057';
+export const md_pink_A700 = '#C51162';
+
+// Purple
+export const md_purple_50 = '#F3E5F5';
+export const md_purple_100 = '#E1BEE7';
+export const md_purple_200 = '#CE93D8';
+export const md_purple_300 = '#BA68C8';
+export const md_purple_400 = '#AB47BC';
+export const md_purple_500 = '#9C27B0';
+export const md_purple_600 = '#8E24AA';
+export const md_purple_700 = '#7B1FA2';
+export const md_purple_800 = '#6A1B9A';
+export const md_purple_900 = '#4A148C';
+export const md_purple_A100 = '#EA80FC';
+export const md_purple_A200 = '#E040FB';
+export const md_purple_A400 = '#D500F9';
+export const md_purple_A700 = '#AA00FF';
+
+// Deep Purple
+export const md_deep_purple_50 = '#EDE7F6';
+export const md_deep_purple_100 = '#D1C4E9';
+export const md_deep_purple_200 = '#B39DDB';
+export const md_deep_purple_300 = '#9575CD';
+export const md_deep_purple_400 = '#7E57C2';
+export const md_deep_purple_500 = '#673AB7';
+export const md_deep_purple_600 = '#5E35B1';
+export const md_deep_purple_700 = '#512DA8';
+export const md_deep_purple_800 = '#4527A0';
+export const md_deep_purple_900 = '#311B92';
+export const md_deep_purple_A100 = '#B388FF';
+export const md_deep_purple_A200 = '#7C4DFF';
+export const md_deep_purple_A400 = '#651FFF';
+export const md_deep_purple_A700 = '#6200EA';
