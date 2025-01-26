9a704097116dc782923ceb0e7a7da0d271a082bd
Sid <tosid23@gmail.com>
2025-01-26 17:00:05 +0530

    Refactor layout and Sign-In screen to improve structure and styling; hide header and use SafeAreaView with ScrollView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
-import "./globals.css"
+import "./globals.css";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Text>sign-in</Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
