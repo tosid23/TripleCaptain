6e365c1ab20df58ae252ffc6f7f95904ebc7608d
Sid <tosid23@gmail.com>
2025-01-26 16:38:05 +0530

    Update app.json for splash screen settings, adjust Tailwind CSS content paths, and add Material Design color constants

---
+++ constants/Colors.ts
@@ -0,0 +1,306 @@
+/**
+ * Google's Material Design Colors
+ * From: http://www.google.com/design/spec/style/color.html#color-ui-color-palette
+ *
+ * For use in light and dark themes (where applicable)
+ */
+
+// Red
+export const md_red_50 = '#FFEBEE';
+export const md_red_100 = '#FFCDD2';
+export const md_red_200 = '#EF9A9A';
+export const md_red_300 = '#E57373';
+export const md_red_400 = '#EF5350';
+export const md_red_500 = '#F44336';
+export const md_red_600 = '#E53935';
+export const md_red_700 = '#D32F2F';
+export const md_red_800 = '#C62828';
+export const md_red_900 = '#B71C1C';
+export const md_red_A100 = '#FF8A80';
+export const md_red_A200 = '#FF5252';
+export const md_red_A400 = '#FF1744';
+export const md_red_A700 = '#D50000';
+
+// Pink
+export const md_pink_50 = '#FCE4EC';
+export const md_pink_100 = '#F8BBD0';
+export const md_pink_200 = '#F48FB1';
+export const md_pink_300 = '#F06292';
+export const md_pink_400 = '#EC407A';
+export const md_pink_500 = '#E91E63';
+export const md_pink_600 = '#D81B60';
+export const md_pink_700 = '#C2185B';
+export const md_pink_800 = '#AD1457';
+export const md_pink_900 = '#880E4F';
+export const md_pink_A100 = '#FF80AB';
+export const md_pink_A200 = '#FF4081';
+export const md_pink_A400 = '#F50057';
+export const md_pink_A700 = '#C51162';
+
+// Purple
+export const md_purple_50 = '#F3E5F5';
+export const md_purple_100 = '#E1BEE7';
+export const md_purple_200 = '#CE93D8';
+export const md_purple_300 = '#BA68C8';
+export const md_purple_400 = '#AB47BC';
+export const md_purple_500 = '#9C27B0';
+export const md_purple_600 = '#8E24AA';
+export const md_purple_700 = '#7B1FA2';
+export const md_purple_800 = '#6A1B9A';
+export const md_purple_900 = '#4A148C';
+export const md_purple_A100 = '#EA80FC';
+export const md_purple_A200 = '#E040FB';
+export const md_purple_A400 = '#D500F9';
+export const md_purple_A700 = '#AA00FF';
+
+// Deep Purple
+export const md_deep_purple_50 = '#EDE7F6';
+export const md_deep_purple_100 = '#D1C4E9';
+export const md_deep_purple_200 = '#B39DDB';
+export const md_deep_purple_300 = '#9575CD';
+export const md_deep_purple_400 = '#7E57C2';
+export const md_deep_purple_500 = '#673AB7';
+export const md_deep_purple_600 = '#5E35B1';
+export const md_deep_purple_700 = '#512DA8';
+export const md_deep_purple_800 = '#4527A0';
+export const md_deep_purple_900 = '#311B92';
+export const md_deep_purple_A100 = '#B388FF';
+export const md_deep_purple_A200 = '#7C4DFF';
+export const md_deep_purple_A400 = '#651FFF';
+export const md_deep_purple_A700 = '#6200EA';
+
+// Indigo
+export const md_indigo_50 = '#E8EAF6';
+export const md_indigo_100 = '#C5CAE9';
+export const md_indigo_200 = '#9FA8DA';
+export const md_indigo_300 = '#7986CB';
+export const md_indigo_400 = '#5C6BC0';
+export const md_indigo_500 = '#3F51B5';
+export const md_indigo_600 = '#3949AB';
+export const md_indigo_700 = '#303F9F';
+export const md_indigo_800 = '#283593';
+export const md_indigo_900 = '#1A237E';
+export const md_indigo_A100 = '#8C9EFF';
+export const md_indigo_A200 = '#536DFE';
+export const md_indigo_A400 = '#3D5AFE';
+export const md_indigo_A700 = '#304FFE';
+
+// Blue
+export const md_blue_50 = '#E3F2FD';
+export const md_blue_100 = '#BBDEFB';
+export const md_blue_200 = '#90CAF9';
+export const md_blue_300 = '#64B5F6';
+export const md_blue_400 = '#42A5F5';
+export const md_blue_500 = '#2196F3';
+export const md_blue_600 = '#1E88E5';
+export const md_blue_700 = '#1976D2';
+export const md_blue_800 = '#1565C0';
+export const md_blue_900 = '#0D47A1';
+export const md_blue_A100 = '#82B1FF';
+export const md_blue_A200 = '#448AFF';
+export const md_blue_A400 = '#2979FF';
+export const md_blue_A700 = '#2962FF';
+
+// Light Blue
+export const md_light_blue_50 = '#E1F5FE';
+export const md_light_blue_100 = '#B3E5FC';
+export const md_light_blue_200 = '#81D4fA';
+export const md_light_blue_300 = '#4fC3F7';
+export const md_light_blue_400 = '#29B6FC';
+export const md_light_blue_500 = '#03A9F4';
+export const md_light_blue_600 = '#039BE5';
+export const md_light_blue_700 = '#0288D1';
+export const md_light_blue_800 = '#0277BD';
+export const md_light_blue_900 = '#01579B';
+export const md_light_blue_A100 = '#80D8FF';
+export const md_light_blue_A200 = '#40C4FF';
+export const md_light_blue_A400 = '#00B0FF';
+export const md_light_blue_A700 = '#0091EA';
+
+// Cyan
+export const md_cyan_50 = '#E0F7FA';
+export const md_cyan_100 = '#B2EBF2';
+export const md_cyan_200 = '#80DEEA';
+export const md_cyan_300 = '#4DD0E1';
+export const md_cyan_400 = '#26C6DA';
+export const md_cyan_500 = '#00BCD4';
+export const md_cyan_600 = '#00ACC1';
+export const md_cyan_700 = '#0097A7';
+export const md_cyan_800 = '#00838F';
+export const md_cyan_900 = '#006064';
+export const md_cyan_A100 = '#84FFFF';
+export const md_cyan_A200 = '#18FFFF';
+export const md_cyan_A400 = '#00E5FF';
+export const md_cyan_A700 = '#00B8D4';
+
+// Teal
+export const md_teal_50 = '#E0F2F1';
+export const md_teal_100 = '#B2DFDB';
+export const md_teal_200 = '#80CBC4';
+export const md_teal_300 = '#4DB6AC';
+export const md_teal_400 = '#26A69A';
+export const md_teal_500 = '#009688';
+export const md_teal_600 = '#00897B';
+export const md_teal_700 = '#00796B';
+export const md_teal_800 = '#00695C';
+export const md_teal_900 = '#004D40';
+export const md_teal_A100 = '#A7FFEB';
+export const md_teal_A200 = '#64FFDA';
+export const md_teal_A400 = '#1DE9B6';
+export const md_teal_A700 = '#00BFA5';
+
+// Green
+export const md_green_50 = '#E8F5E9';
+export const md_green_100 = '#C8E6C9';
+export const md_green_200 = '#A5D6A7';
+export const md_green_300 = '#81C784';
+export const md_green_400 = '#66BB6A';
+export const md_green_500 = '#4CAF50';
+export const md_green_600 = '#43A047';
+export const md_green_700 = '#388E3C';
+export const md_green_800 = '#2E7D32';
+export const md_green_900 = '#1B5E20';
+export const md_green_A100 = '#B9F6CA';
+export const md_green_A200 = '#69F0AE';
+export const md_green_A400 = '#00E676';
+export const md_green_A700 = '#00C853';
+
+// Light Green
+export const md_light_green_50 = '#F1F8E9';
+export const md_light_green_100 = '#DCEDC8';
+export const md_light_green_200 = '#C5E1A5';
+export const md_light_green_300 = '#AED581';
+export const md_light_green_400 = '#9CCC65';
+export const md_light_green_500 = '#8BC34A';
+export const md_light_green_600 = '#7CB342';
+export const md_light_green_700 = '#689F38';
+export const md_light_green_800 = '#558B2F';
+export const md_light_green_900 = '#33691E';
+export const md_light_green_A100 = '#CCFF90';
+export const md_light_green_A200 = '#B2FF59';
+export const md_light_green_A400 = '#76FF03';
+export const md_light_green_A700 = '#64DD17';
+
+// Lime
+export const md_lime_50 = '#F9FBE7';
+export const md_lime_100 = '#F0F4C3';
+export const md_lime_200 = '#E6EE9C';
+export const md_lime_300 = '#DCE775';
+export const md_lime_400 = '#D4E157';
+export const md_lime_500 = '#CDDC39';
+export const md_lime_600 = '#C0CA33';
+export const md_lime_700 = '#A4B42B';
+export const md_lime_800 = '#9E9D24';
+export const md_lime_900 = '#827717';
+export const md_lime_A100 = '#F4FF81';
+export const md_lime_A200 = '#EEFF41';
+export const md_lime_A400 = '#C6FF00';
+export const md_lime_A700 = '#AEEA00';
+
+// Yellow
+export const md_yellow_50 = '#FFFDE7';
+export const md_yellow_100 = '#FFF9C4';
+export const md_yellow_200 = '#FFF590';
+export const md_yellow_300 = '#FFF176';
+export const md_yellow_400 = '#FFEE58';
+export const md_yellow_500 = '#FFEB3B';
+export const md_yellow_600 = '#FDD835';
+export const md_yellow_700 = '#FBC02D';
+export const md_yellow_800 = '#F9A825';
+export const md_yellow_900 = '#F57F17';
+export const md_yellow_A100 = '#FFFF82';
+export const md_yellow_A200 = '#FFFF00';
+export const md_yellow_A400 = '#FFEA00';
+export const md_yellow_A700 = '#FFD600';
+
+// Amber
+export const md_amber_50 = '#FFF8E1';
+export const md_amber_100 = '#FFECB3';
+export const md_amber_200 = '#FFE082';
+export const md_amber_300 = '#FFD54F';
+export const md_amber_400 = '#FFCA28';
+export const md_amber_500 = '#FFC107';
+export const md_amber_600 = '#FFB300';
+export const md_amber_700 = '#FFA000';
+export const md_amber_800 = '#FF8F00';
+export const md_amber_900 = '#FF6F00';
+export const md_amber_A100 = '#FFE57F';
+export const md_amber_A200 = '#FFD740';
+export const md_amber_A400 = '#FFC400';
+export const md_amber_A700 = '#FFAB00';
+
+// Orange
+export const md_orange_50 = '#FFF3E0';
+export const md_orange_100 = '#FFE0B2';
+export const md_orange_200 = '#FFCC80';
+export const md_orange_300 = '#FFB74D';
+export const md_orange_400 = '#FFA726';
+export const md_orange_500 = '#FF9800';
+export const md_orange_600 = '#FB8C00';
+export const md_orange_700 = '#F57C00';
+export const md_orange_800 = '#EF6C00';
+export const md_orange_900 = '#E65100';
+export const md_orange_A100 = '#FFD180';
+export const md_orange_A200 = '#FFAB40';
+export const md_orange_A400 = '#FF9100';
+export const md_orange_A700 = '#FF6D00';
+
+// Deep Orange
+export const md_deep_orange_50 = '#FBE9E7';
+export const md_deep_orange_100 = '#FFCCBC';
+export const md_deep_orange_200 = '#FFAB91';
+export const md_deep_orange_300 = '#FF8A65';
+export const md_deep_orange_400 = '#FF7043';
+export const md_deep_orange_500 = '#FF5722';
+export const md_deep_orange_600 = '#F4511E';
+export const md_deep_orange_700 = '#E64A19';
+export const md_deep_orange_800 = '#D84315';
+export const md_deep_orange_900 = '#BF360C';
+export const md_deep_orange_A100 = '#FF9E80';
+export const md_deep_orange_A200 = '#FF6E40';
+export const md_deep_orange_A400 = '#FF3D00';
+export const md_deep_orange_A700 = '#DD2C00';
+
+// Brown
+export const md_brown_50 = '#EFEBE9';
+export const md_brown_100 = '#D7CCC8';
+export const md_brown_200 = '#BCAAA4';
+export const md_brown_300 = '#A1887F';
+export const md_brown_400 = '#8D6E63';
+export const md_brown_500 = '#795548';
+export const md_brown_600 = '#6D4C41';
+export const md_brown_700 = '#5D4037';
+export const md_brown_800 = '#4E342E';
+export const md_brown_900 = '#3E2723';
+
+// Grey
+export const md_grey_50 = '#FAFAFA';
+export const md_grey_100 = '#F5F5F5';
+export const md_grey_200 = '#EEEEEE';
+export const md_grey_300 = '#E0E0E0';
+export const md_grey_400 = '#BDBDBD';
+export const md_grey_500 = '#9E9E9E';
+export const md_grey_600 = '#757575';
+export const md_grey_700 = '#616161';
+export const md_grey_800 = '#424242';
+export const md_grey_900 = '#212121';
+
+// Blue Grey
+export const md_blue_grey_50 = '#ECEFF1';
+export const md_blue_grey_100 = '#CFD8DC';
+export const md_blue_grey_200 = '#B0BBC5';
+export const md_blue_grey_300 = '#90A4AE';
+export const md_blue_grey_400 = '#78909C';
+export const md_blue_grey_500 = '#607D8B';
+export const md_blue_grey_600 = '#546E7A';
+export const md_blue_grey_700 = '#455A64';
+export const md_blue_grey_800 = '#37474F';
+export const md_blue_grey_900 = '#263238';
+
+// Black and White
+export const md_black_1000 = '#000000';
+export const md_white_1000 = '#FFFFFF';
+
+// Example of tint colors for light/dark themes:
+export const tintColorLight = '#0a7ea4';
+export const tintColorDark = '#fff';
