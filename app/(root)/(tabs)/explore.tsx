b679fa4eae68073687f97d2225050f0ba061814f
Sid <tosid23@gmail.com>
2025-01-27 09:42:52 +0530

    Refactor Explore, Profile, and Sign-In screens to use SafeAreaView and ScrollView; add back button functionality with ArrowLeftIcon

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,11 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ArrowLeftIcon } from "react-native-heroicons/outline";
 
 const Explore = () => {
   return (
-    <View>
-      <Text>explore</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+          <View className="flex-row items-center p-4">
+            {/* Back Button */}
+            <TouchableOpacity className="p-2">
+              <ArrowLeftIcon size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+          <ScrollView contentContainerClassName="flex-1 justify-center items-center">
+            <Text className="text-lg font-semibold">Explore</Text>
+          </ScrollView>
+        </SafeAreaView>
   )
 }
 
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,19 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ArrowLeftIcon } from "react-native-heroicons/outline";
 
 const SignIn = () => {
   return (
     <SafeAreaView className="bg-white h-full">
+      <View className="flex-row items-center p-4">
+        {/* Back Button */}
+        <TouchableOpacity className="p-2">
+          <ArrowLeftIcon size={24} color="black" />
+        </TouchableOpacity>
+      </View>
       <ScrollView contentContainerClassName="flex-1 justify-center items-center">
-          <Text className="text-lg font-semibold">Sign In</Text>
+        <Text className="text-lg font-semibold">Sign In</Text>
       </ScrollView>
     </SafeAreaView>
   );
